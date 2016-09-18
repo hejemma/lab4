596747127709fe9c330321fe888fe6fe4746944a
hejemma <emmwa513@student.liu.se>
2016-09-18 16:51:16 +0200

    plot 1

---
+++ multireg/R/plot.R
@@ -0,0 +1,14 @@
+
+plot.linreg<-function(argument){
+  plotdf<-data.frame(Fitted=argument$fitted,
+                     Residuals=argument$residuals)# select the relevant data and make df
+  #coefdf<-data.frame(Coef=argument$coefficients)  
+  
+  p<- ggplot(plotdf)
+  p<- p + geom_point(aes(x=Fitted, y=Residuals)) + ggtitle("Residual vs Fitted Plot")+theme_bw()
+  p<- p + stat_smooth(method="loess") + 
+    geom_hline(yintercept=0, col="black", linetype="dashed") # the dashed black line
+  p
+}
+
+plot(listhelv)
