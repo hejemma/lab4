bc911c46ada28fbe68762ed27b0a3a80a59df9a2
ZaidaLiendeborg <zaida@liendeborg.se>
2016-09-19 11:57:02 +0200

    Tar bort raden med UseMethod

--- multireg/R/pred.R
+++ multireg/R/pred.R
@@ -9,8 +9,6 @@
 #' @export
 
 
-pred<- function(x) UseMethod("pred")
-
 pred.linreg<- function(x){
   pred<- as.vector(x$"fitted values")
   return(pred)
--- multireg/R/resid.R
+++ multireg/R/resid.R
@@ -1,5 +1,3 @@
-resid<- function(x) UseMethod("resid")
-
 resid.linreg<- function(x){
   resid<- as.vector(x$residuals)
   return(resid)
