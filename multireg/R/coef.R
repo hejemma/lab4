cd5ccb8af2728e80a63b06299290066efd88e9e8
hejemma <emmwa513@student.liu.se>
2016-09-19 10:43:02 +0200

    låt se

--- multireg/R/coef.R
+++ multireg/R/coef.R
@@ -3,6 +3,12 @@
 #' Returning the beta estimates of the regression model calculated by the \code{\link{linreg}} function.
 #' @param See \code{\link{linreg}}
 #' @return A named vector.
+#' Extract Beta Coefficients.
+#' 
+#' \code{coef} returns the estimated beta coefficients of the regression model calculated by the \code{\link{linreg}} function.
+#' @param See \code{\link{linreg}}
+#' @return Coefficients extracted from the model \code{x} and returned as a named vector.
+
 #' @examples
 #' x<- linreg(formula= Sepal.Width ~ Sepal.Length, data= iris)
 #' coef(x)
