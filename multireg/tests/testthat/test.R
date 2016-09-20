91623a9b261a1790b5e10185c36b1554abeae542
ZaidaLiendeborg <zaida@liendeborg.se>
2016-09-20 12:38:51 +0200

    added tests

--- multireg/tests/testthat/test.R
+++ multireg/tests/testthat/test.R
@@ -3,6 +3,7 @@ test_that("Testing linreg()", {
   formel<-Petal.Length~Sepal.Width
   model<- linreg(formula=formel, data=iris)
   stderr<- sqrt(diag(model$variance_of_regression_coefficent))
+  tval<-as.vector(model$tvalue)
   
   lmodel<- lm(formula= formel, data= iris)
       
@@ -13,14 +14,21 @@ test_that("Testing linreg()", {
   
   expect_is(model, "linreg", info = "Error: Returned value is not of linreg class.")
   
-  expect_equal(model$coefficients, as.matrix(lmodel$coefficients), 
+  expect_equal(coef(model), lmodel$coefficients, 
                info="Error: Coefficients are not correct.")
   
-  expect_equal(model$residuals, as.vector(lmodel$residuals),
+  expect_equal(resid(model), as.vector(lmodel$residuals),
                info="Error: Coefficients are not correct.")
   
-  expect_equal(model$fitted, as.matrix(lmodel$fitted.values), 
+  expect_equal(predict(model), as.vector(lmodel$fitted.values), 
                info="Error: Coefficients are not correct.")
+
+  expect_equal(stderr, summary(lmodel)$coefficients[,2], tolerance= 0.05,
+               info="Error: Coefficients are not correct.")
+  
+  expect_equal(tval, as.vector(summary(lmodel)$coefficients[,3]), tolerance= 0.05,
+               info="Error: Coefficients are not correct.")
+  
   }
 )
 
