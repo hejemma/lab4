bb6d2cedec62eb1fce27acea5379bc8549d28a2a
hejemma <emmwa513@student.liu.se>
2016-10-12 13:13:14 +0200

    predict ridgereg

---
+++ multireg/R/predictridgereg.R
@@ -0,0 +1,25 @@
+#' Extract Fitted Values from linreg object.
+#' 
+#' \code{pred} returns the fitted values of the regression model calculated by the \code{\link{ridgereg}} function.
+#' @param object An object of class \code{ridgereg} 
+#' @param newdata If another data set with same variables is wished to use for prediction
+#' @param ... other arguments like .
+#' @return A numeric vector of fitted values extracted from the model in \code{\link{ridgereg}}.
+#'
+#' @examples
+#' x<- ridgereg(formula= Sepal.Width ~ Sepal.Length, data= iris)
+#' predict(x)
+#' @export
+
+
+
+predict.ridgereg<- function(object, newdata,...){
+  if(missing(newdata)) {
+    predd<-as.vector(object$Fitted_values)
+  }else{
+    pred1<-ridgereg(formula=object$Formula, data=newdata)
+    predd<-pred1$Fitted_values
+  }
+  return(predd)
+  
+  }
