248f9093e82e7f0a2c07b628c6d98006cdd3a156
ZaidaLiendeborg <zaida@liendeborg.se>
2016-10-14 12:37:26 +0200

    Fixade bug på visualize...

--- multireg/R/visualize_airport_delays.R
+++ multireg/R/visualize_airport_delays.R
@@ -8,47 +8,41 @@
 
 
 visualize_airports_delay<- function(){
-  
 library(dplyr)
 library(ggplot2)
 library(nycflights13)  
 
 # subsetting the important variables for the analysis #
-airports_sub<- select(airports, faa, name, lat, lon)
-flights_sub<- select(flights, dest, dep_delay, arr_delay)
+airports_sub<- dplyr::select(airports, faa, name, lat, lon)
+flights_sub<- dplyr::select(flights, dest, dep_delay, arr_delay)
 
 # Total time of delays #
-new_flights<- mutate(flights_sub, total_delay = dep_delay + arr_delay)
-
-# Filtering NAs#
+new_flights<- dplyr::mutate(flights_sub, total_delay = dep_delay + arr_delay)
 
 # Merging the two data frames #
-airFlight<- inner_join(airports_sub, new_flights, by=c("faa"="dest"))
+airFlight<- dplyr::inner_join(airports_sub, new_flights, by=c("faa"="dest"))
 
 # Compute total mean delay of flights by airport #
 mean_delay<- airFlight %>% group_by(faa) %>% summarise(total_mean = mean(total_delay, na.rm=T))
 
 # Creating new data set for plotting. Total mean delay for every airport #
-airFlight_delay<- inner_join(airports_sub, mean_delay, by="faa")
+airFlight_delay<- dplyr::inner_join(airports_sub, mean_delay, by="faa")
 
 # To get states name we use function latlong2state # 
 airFlight_delay$States<- latlong2state(airFlight_delay[,c("lon", "lat")])
 
-airFlight_delay <- airFlight_delay %>% select(States,total_mean, lon, lat) 
+airFlight_delay <- airFlight_delay %>% select(States, total_mean, lon, lat) 
 
 # Plotting the data #
-world<- map_data("world")
+world<- ggplot2::map_data("world")
 
-USA<- filter(world, region == c("USA", "Canada"))
-
+USA<- dplyr::filter(world, region == c("USA", "Canada"))
 
 map_base<- ggplot() + coord_cartesian(xlim=c(-160,-45), ylim=c(min(USA$lat),65), expand=F)+
   geom_polygon(data = USA, mapping = aes(x = long, y = lat, group=group),color = "gray", fill = "white") 
   
-
 plot<- map_base + geom_point(data = airFlight_delay, aes(x= lon, y=lat, color=total_mean, size=total_mean)) +
   ggtitle("Mean total flight delays for different airports")
-
 
 plot
 
